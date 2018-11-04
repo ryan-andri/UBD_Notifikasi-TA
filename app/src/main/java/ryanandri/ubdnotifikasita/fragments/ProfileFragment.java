b08b085e9b7e8a79c2c43132e1d3200802eb6d85
Ryan Andri <ryanandri@linuxmail.org>
2018-11-04 13:21:50 +0700

    add button logout listener
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- app/src/main/java/ryanandri/ubdnotifikasita/fragments/ProfileFragment.java
+++ app/src/main/java/ryanandri/ubdnotifikasita/fragments/ProfileFragment.java
@@ -1,22 +1,62 @@
 package ryanandri.ubdnotifikasita.fragments;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import ryanandri.ubdnotifikasita.LoginActivity;
 import ryanandri.ubdnotifikasita.R;
 
 public class ProfileFragment extends Fragment {
+    private Button buttonLogout;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, null);
+
+        buttonLogout = view.findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        logout();
+                    }
+                }
+        );
+
         return view;
+    }
+
+    public void logout() {
+        AlertDialog.Builder dialogLogout = new AlertDialog.Builder(getActivity());
+        dialogLogout.setMessage("Anda ingin logout ?");
+        dialogLogout.setPositiveButton("iya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+        dialogLogout.setNegativeButton("tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //
+            }
+        });
+
+        AlertDialog alertDialog = dialogLogout.create();
+        alertDialog.show();
     }
 }
