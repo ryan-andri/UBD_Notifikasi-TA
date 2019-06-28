d674824e1a1bad0dd402d060dc57f9673ef56b1b
Ryan Andri <ryanandri@linuxmail.org>
2019-06-28 18:46:36 +0700

    update tampilan expand notifikasi layout

--- app/src/main/java/ryanandri/ubdnotifikasita/session/Constant.java
+++ app/src/main/java/ryanandri/ubdnotifikasita/session/Constant.java
@@ -1,7 +1,7 @@
 package ryanandri.ubdnotifikasita.session;
 
 public class Constant {
-    public static final String URL = "http://192.168.1.8/panel_notifikasi_ta_ubd/api/";
+    public static final String URL = "http://192.168.1.2/panel_notifikasi_ta_ubd/api/";
     public static final String login = "login.php";
     public static final String ajukan_judul = "pengajuan_judul.php";
     public static final String ambil_data_judul = "ambil_data_judul.php";
